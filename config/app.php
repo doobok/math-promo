9048ba496e8720b6b87e724bea31b4a11068adee
yura <yura.chernobay@gmail.com>
2021-10-04 14:36:01 +0300

    Add GA code

--- config/app.php
+++ config/app.php
@@ -12,6 +12,8 @@ return [
     | any other location as required by the application or its packages.
     |
     */
+    'ga' => env('GA_CODE', null),
+
 
     'name' => env('APP_NAME', 'Laravel'),
 
--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,11 +1,11 @@
 {{-- Global site tag (gtag.js) - Google Analytics --}}
-<script async src="https://www.googletagmanager.com/gtag/js?id={{config('var.ga')}}"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{config('app.ga')}}"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
 
- gtag('config', '{{config('var.ga')}}');
+ gtag('config', '{{config('app.ga')}}');
 </script>
 
 {{-- @include('layouts.partials.hreflang') --}}
