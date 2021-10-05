e275ea4deb2d4a44165e0ea620aab2bfc60bc08e
yura <yura.chernobay@gmail.com>
2021-10-05 15:17:28 +0300

    Fix robots.txt

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::view('/', 'policy');
 Route::view('/policy', 'policy');
 
 Route::get('/{slug}', 'App\Http\Controllers\PagesController@promoPage');
