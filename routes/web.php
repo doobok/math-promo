8925287d59d29b4d8c90522ff7370f46f56a5560
yura <yura.chernobay@gmail.com>
2021-10-05 16:22:52 +0300

    Small fixes

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('/', 'policy');
 Route::view('/policy', 'policy');
 
 Route::get('/{slug}', 'App\Http\Controllers\PagesController@promoPage');
